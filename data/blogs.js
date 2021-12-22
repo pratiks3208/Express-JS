2b65ae0b49a0a396a2336b62ef38ae14de44d60a
Pratik Singh <73792866+pratiks3208@users.noreply.github.com>
2021-12-22 09:45:51 +0530

    Add files via upload

---
+++ data/blogs.js
@@ -0,0 +1,29 @@
+blogs = [
+        {
+            title: 'How to get started with python',
+            content: 'This is content1',
+            slug: "python-learn"
+        },
+        {
+            title: 'How to get started with JS',
+            content: 'This is content2',
+            slug: "JS-learn"
+        },
+        {
+            title: 'How to get started with Django',
+            content: 'This is content3',
+            slug: "Django-learn"
+        },
+        {
+            title: 'How to get started with HTML',
+            content: 'This is content4',
+            slug: "HTML-learn"
+        },
+        {
+            title: 'How to get started with CSS',
+            content: 'This is content5',
+            slug: "CSS-learn"
+        }
+    ]
+
+module.exports = blogs;
---
+++ routes/blog.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const path = require('path')
+const blogs = require('../data/blogs')
+
+const router = express.Router()
+
+router.get('/', (req,res)=>{
+    // res.sendFile(path.join(__dirname , '../templates/index.html'))
+    // app.get('/', function (req, res) {
+        res.render('home');
+    // });
+})
+
+router.get('/blog', (req,res)=>{
+    // blogs.forEach(e => {
+    //     console.log(e.title)
+    // })
+    // res.sendFile(path.join(__dirname , '../views/blogHome.html'))
+    res.render('blogHome',{
+        blogs : blogs
+    });
+})
+
+router.get('/blogpost/:slug', (req,res)=>{
+    myBlog = blogs.filter((e)=>{
+        return e.slug == req.params.slug
+    })
+    // console.log(myBlog)
+    res.render('blogPage',{
+        title: myBlog[0].title,
+        content: myBlog[0].content
+    });
+    // console.log(myBlog)
+    // res.sendFile(path.join(__dirname , '../views/blogPage.html'))
+
+})
+
+module.exports = router
